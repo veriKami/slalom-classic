46802c37f8a015123acd54a15764953cae5c28dd
Weronika Kami <weronika@nyorai.tv>
2013-02-22 08:26:32 +0100

    php: refactoring

--- judgment/setstart.php
+++ judgment/setstart.php
@@ -1,8 +1,17 @@
-<?
-require ('check.inc');
+<?php //:
+////////:
+
+require('check.inc');
 require('../connect.php');
-if ($_SESSION['np']) {$r=mysql_query('Update skaters Set ttimeb='.($_GET['s']?'NOW(), ttimel='.$_GET['time']:'null, ttimel=null').' Where id='.$_GET['id']);}
-if ($r) echo '1';
- else echo '0';
+
+if ($_SESSION['np']) {
+    $r=mysql_query('Update skaters Set ttimeb='.($_GET['s']?'NOW(), ttimel='.$_GET['time']:'null, ttimel=null').' Where id='.$_GET['id']);
+}
+
+if ($r) {
+    echo '1';
+} else {
+    echo '0';
+}
 
 ?>
