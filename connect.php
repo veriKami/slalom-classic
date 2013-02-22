b5736adcbc297bfc5158994189e56007e38ad99e
Weronika Kami <weronika@nyorai.tv>
2013-02-22 09:57:17 +0100

    db: veriKami's localhost

--- connect.php
+++ connect.php
@@ -1,9 +1,19 @@
-<?
+<?php //:
+////////:
+
 setlocale(LC_ALL, "ru_RU.UTF8");
 $dbhost = '127.0.0.1';
-$dbname = 'slalom';
-$dbuser = 'slalom';
-$dbpasswd = '12345678';
-mysql_connect($dbhost,$dbuser,$dbpasswd); 
+
+//$dbname = 'slalom';
+//$dbuser = 'slalom';
+//$dbpasswd = '12345678';
+
+//: veriKami's local
+$dbname   = 'gh_slalom-classic';
+$dbuser   = 'root';
+$dbpasswd = 'root';
+
+mysql_connect($dbhost, $dbuser, $dbpasswd); 
 mysql_select_db($dbname);
+
 ?>
